e1b8beb2afded9f163ff5c256540f5ecd708423c
wndudtjd <dudtjdwn1575@naver.com>
2023-06-12 01:52:17 +0900

    특정 게시물 가져오기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -7,7 +7,7 @@ import { CreateBoardDto } from './dto/create-board.dto';
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
 
-    @Get()
+    @Get('/')
     getAllBoards(): Board[] {
         return this.boardsService.getAllBoards();
     }
@@ -17,5 +17,10 @@ export class BoardsController {
         @Body() createBoardDto: CreateBoardDto
     ): Board {
         return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id)
     }
 }
